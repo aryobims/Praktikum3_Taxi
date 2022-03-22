0a0c561367aea7eb0ec6f631ccaab6be3b948280
aryobims <aryobimo@students.amikom.ac.id>
2022-03-22 21:38:49 +0700

    inisialisasi project taxi

---
+++ TugasTaxi/TugasTaxi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            // pengesetan nilai properties
+            taxi.DriverName = "Jono";
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+
+            if (taxi.OnDuty = true) 
+            {
+                taxi.PickUpPassenger(); //pemanggilan method jika taxi.OnDuty = true  
+                taxi.NumPassenger = 10;
+            } else {
+                taxi.DropOffPassenger(); //pemanggilan method jika taxi.OnDuty = false 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
